0f02ab634b5cfea87470dc36f7e30b161d0b5e35
Sebastian Johansson <johansson.sebastian@gmail.com>
2015-08-16 11:27:03 +0000

    cleaned up the code a bit

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
-
 var express = require('express');
 var app = express();
 
 var sleep = require('sleep');
-
-//var pigpio = require("pi-gpio");
 
 var Gpio = require('onoff').Gpio,
   upButton = new Gpio(18, 'out'),
@@ -14,100 +11,76 @@ var Gpio = require('onoff').Gpio,
 
   stopButton.writeSync(0);
   upButton.writeSync(0);
-//  downButton.writeSync(0);
+  downButton.writeSync(0);
   selectButton.writeSync(0);
 
 var standardButtonPress = 60000; //microseconds
 
-app.post('/', function(req, res) {
-  res.type('text/plain');
-  res.send('SomfyProxy');
+app.get('/', function(req, res) {
+  	res.type('text/plain');
+  	res.send('');
 });
 
 app.post('/up', function(req, res) {
-  res.type('text/plain');
-  res.send('going up');
+	res.type('text/plain');
+  	res.send('going up');
 
-upButton.writeSync(1);
-sleep.usleep(standardButtonPress);
-upButton.writeSync(0);
+	upButton.writeSync(1);
+	sleep.usleep(standardButtonPress);
+	upButton.writeSync(0);
                   
 });
 
 app.post('/down', function(req, res) {
-  res.type('text/plain');
-  res.send('going down');
+  	res.type('text/plain');
+  	res.send('going down');
 
-console.log('setting down to 1')
-downButton.setDirection('out');
-downButton.writeSync(1);
-sleep.usleep(60000);
-downButton.writeSync(0);
-downButton.setDirection('in');
-
-/*downButton.write(1,function(err){
-	console.log(err);
-	sleep.usleep(200000);
-	downButton.write(0,function(err){
-	console.log(err);
-	});
-});*/
-//sleep.sleep(5);
-//console.log('setting down to 0');
-//downButton.writeSync(0);
-
-
-//pigpio.open(8, "output", function(err) {     // Open pin 16 for output
-//	console.log(err);
-//         pigpio.write(8, 1, function() {        
-//setTimeout(function(){pigpio.write(8, 0, function() { pigpio.close(8);},1000);
-//  });
-//});
-//});
+	downButton.writeSync(1);
+	sleep.usleep(standardButtonPress);
+	downButton.writeSync(0);
    
 });
 
 app.post('/stop', function(req, res) {
-  res.type('text/plain');
-  res.send('stopping');
+  	res.type('text/plain');
+  	res.send('stopping');
 
-stopButton.writeSync(1);
-sleep.usleep(standardButtonPress);
-stopButton.writeSync(0);
+	stopButton.writeSync(1);
+	sleep.usleep(standardButtonPress);
+	stopButton.writeSync(0);
 
 });
 
 app.post('/select', function(req, res) {
-  res.type('text/plain');
-  res.send('changed channel');
+  	res.type('text/plain');
+  	res.send('changed channel');
 
-selectChannel();
+	selectChannel();
 });
-
 
 function selectChannel()
 {
-selectButton.writeSync(1);
-sleep.sleep(2);
-selectButton.writeSync(0);
+	selectButton.writeSync(1);
+	sleep.sleep(2);
+	selectButton.writeSync(0);
 }
 
 app.listen(4747);
 
 function exit() {
-  stopButton.writeSync(0);
-  stopButton.unexport();
+  	stopButton.writeSync(0);
+  	stopButton.unexport();
 
-  upButton.writeSync(0);
-  upButton.unexport();
+  	upButton.writeSync(0);
+  	upButton.unexport();
 
-  //downButton.writeSync(0);
-  //downButton.unexport();
+  	downButton.writeSync(0);
+  	downButton.unexport();
 
-  selectButton.writeSync(0);
-  selectButton.unexport();
+  	selectButton.writeSync(0);
+  	selectButton.unexport();
 
-  process.exit();
+  	process.exit();
 }
 
 process.on('SIGINT', exit);
